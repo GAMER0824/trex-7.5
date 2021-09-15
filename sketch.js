ac02db7d803d93e57833ece5d95980d34684a3ca
GAMER0824 <85885904+GAMER0824@users.noreply.github.com>
2021-09-15 09:28:46 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ function preload() {
 
   jumpSound = loadSound("jump.mp3")
   dieSound = loadSound("die.mp3")
-  checkPointSound = loadSound("checkPoint.mp3")
+  //checkPointSound = loadSound("checkPoint.mp3")
 }
 
 function setup() {
@@ -94,9 +94,9 @@ function draw() {
     score = score + Math.round(getFrameRate() / 60);
     console.log(getFrameRate())
 
-    if (score > 0 && score % 100 === 0) {
-      checkPointSound.play()
-    }
+//     if (score > 0 && score % 100 === 0) {
+//       checkPointSound.play()
+//     }
 
     if (ground.x < 0) {
       ground.x = ground.width / 2;
